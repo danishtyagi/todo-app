f3531824819aa7aafecb27de19c0e9d955e83766
danish tyagi <danishtyagi@yahoo.co>
2023-03-01 19:48:14 +0530

    todo app add functionaliy

--- src/App.js
+++ src/App.js
@@ -11,30 +11,41 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //edit functionality
+    } else {
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
   return (
     <section className="section-center">
-      <div className="grocery-container">
-        <form className="grocery-form" onSubmit={handleSubmit}>
-          {alert.show && <Alert />}
-          <h3>To do App</h3>
-          <div className="form-control">
-            <input
-              type="text"
-              className="grocery"
-              placeholder="eg. learn"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <button type="submit" className="submit-btn">
-              {isEditing ? "edit" : "submit"}
-            </button>
-          </div>
-        </form>
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>To do App</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="eg. learn"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
